7a5d76ee86757523eff9af8877432c13ff5dfc50
Lasse Lundholm <lasslund@outlook.com>
2018-12-03 13:21:29 +0100

    try again

---
+++ RLE_RLD.py
@@ -0,0 +1,46 @@
+def rle_encoder(txt):
+
+    if not txt:
+        return ''
+    c = txt[0]
+    i = 1
+    res = []
+    for x in txt[1:]:
+        if x == c:
+            i += 1
+        else:
+            res.append(f'{c}{i}')
+            i = 1
+            c = x
+    res.append(f'{c}{i}')
+    return ''.join(res)
+
+def rle_decoder(inp):
+    res = []
+    i = 0
+    while i<len(inp):
+        c = inp[i]
+        i += 1
+        count = 0
+        while i<len(inp) and inp[i].isdigit():
+            count = count*10 + int(inp[i])
+            i += 1
+        res.append(c*count)
+
+    return ''.join(res)
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv
+    print(args)
+    if len(args)==3:
+        dat = args[2]
+    elif len(args)==2:
+        dat = sys.stdin.read()
+    else:
+        print("Error")
+        exit(0)
+    if args[1]=='-d':
+        print(rle_decoder(dat))
+    else:
+print(rle_encoder(dat))
---
+++ test_RLE_RLD.py
@@ -0,0 +1,37 @@
+from RLE_RLD import rle_encoder,rle_decoder
+import sys
+from hypothesis import given
+from hypothesis.strategies import text
+#import afl
+
+#afl.init()
+
+def test_simple():
+    assert rle_encoder("bbbkkk") == "b3k3"
+def test_advanced():
+    assert rle_encoder("ffffiiiii") == "f4i5"
+
+def test_single():
+    assert rle_encoder('k') == "k1"
+    assert rle_encoder('kf') == "k1f1"
+
+#  Tests for rle_decoder
+
+def test_simple_decoder():
+    assert rle_decoder('k3b3') == 'kkkbbb'
+
+def test_invariant():
+    for x in [
+        'adfdgdfgfdsg',
+        'asdsassasfgsdfhgjhgfdsdfghjhgfd']:
+        assert rle_decoder(rle_encoder(x)) == x
+
+@given(text())
+def test_hypo(x):
+    print(x)
+    assert rle_decoder(rle_encoder(x)) == x
+
+if __name__ == '__main__':
+    # run 'py-afl-fuzz -o ./pdf/ -i ./examples/ -- (which python) test_rle.py'
+    # from commandline to use afl to fuzz the encoder.
+    print(rle_encoder(sys.stdin.read()))
