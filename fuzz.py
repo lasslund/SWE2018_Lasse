ed8ba4a24b961efae5b366ea3aac3689b47dac53
lasslund <lasslund@outlook.com>
2019-01-09 18:16:01 +0100

    fuzz.py updated, test_string.txt added

--- fuzz.py
+++ fuzz.py
@@ -1,16 +1,11 @@
-rom fuzzing.fuzzer import FuzzExecutor
+from fuzzing.fuzzer import FuzzExecutor
 
 # Files to use as initial input seed.
-file_list = ["./examples/test_string.txt"]
-#["./pdf/2017-12-02_Dagbladet_Information_-_02-12_2017.pdf",
-# "./pdf/CV_eng_2015.pdf", "./pdf/PID3286025.pdf"]
+file_list = ["./test_string.txt"]
+
 
 # List of applications to test.
-apps_under_test = ["python rle.py -e".replace(" ","&")
-                #"/Applications/Adobe Acrobat DC/Adobe Acrobat.app/Contents/MacOS/AdobeAcrobat",
-                   #"/Applications/PDFpen 6.app/Contents/MacOS/PDFpen 6",
-                   #"/Applications/Preview.app/Contents/MacOS/Preview",
-                   ]
+apps_under_test = ["python RLE_RLD.py -e".replace(" ","&")]
 
 number_of_runs = 13
 
@@ -27,4 +22,4 @@ def main():
     print(stats)
 
 if __name__ == "__main__":
-main()
+	main()
